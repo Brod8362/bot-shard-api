7a6fe163ceb765f47d7356f3c4c690f30159cc51
Brod8362 <brod8362@gmail.com>
2022-11-11 21:48:05 -0600

    Add leave endpoint

--- src/shard_api.py
+++ src/shard_api.py
@@ -27,7 +27,6 @@ def shard_ping():
     return "", 200
 
 
-
 @app.get("/status")
 def get_status():
     pool.cull()
@@ -55,3 +54,19 @@ def join_pool():
         "max_shards": pool.max_size
     }
     return response, 200
+
+
+@app.post("/leave")
+def leave_pool():
+    json = request.json
+    if "uuid" not in json:
+        return "UUID not specified", 400
+    if "shard_id" not in json:
+        return "shard ID not specified", 400
+    shard = pool.shards[json["shard_id"]]
+    if shard is None:
+        return "slot already empty", 409
+    if shard.uuid != json["uuid"]:
+        return "uuid mismatch", 403
+    pool.free(json["shard_id"])
+    return "", 200
